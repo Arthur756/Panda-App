97443f3d29dfad5d2c72243821b00a5b34f83dbe
Arthur756 <arthur11gomes@gmail.com>
2021-10-20 19:07:16 -0300

    Atualização da tela Home
    Implementação home page

---
+++ panda-app/Screens/Home.js
@@ -0,0 +1,96 @@
+import React, {userState, useState} from 'react';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+// formik
+import {Formik} from 'formik';
+
+import Tabs from '../Components/Tabs';
+
+import { Content, Card, CardHeader, CardContent, CardFooter,
+Title, Description } from '../Components/Tabs/styles';
+
+//Icons
+import {Octicons, Ionicons, Fontisto} from '@expo/vector-icons';
+
+
+import {
+    StyledContainer,
+    InnerContainer,
+    PageLogo,
+    PageTitle,
+    Subtitle,
+    StyledFormArea,
+    LeftIcon,
+    StyledInputLabel,
+    StyledTextInput,
+    RightIcon,
+    StyledButton,
+    ButtonText,
+    Colors,
+    MsgBox,
+    ExtraView,
+    ExtraText,
+    TextLink,
+    TextLinkContent,
+    WelcomeContainer,
+    Avatar
+
+} from '../Components/styles';
+
+import {View} from 'react-native';
+
+//colors
+const {brand, darkLight, primary, btnred} = Colors;
+
+
+const Home = ({navigation}) => {
+        const [hidePassword, setHidePassword] = useState(true);
+
+    return(
+        
+            <InnerContainer>
+                    <PageLogo realizeMode="cover" source={require('./../assets/panda_logo.png')}/>
+                    <PageTitle> Panda </PageTitle>
+                    <PageTitle welcome={true}> Bem Vindo ao Panda! </PageTitle>
+                    <br/>
+                    <br/>
+                    <Content>
+                        <Card>
+                            <CardHeader>
+                                <Icon name="account-balance-wallet" size={34} color="#07da63"/>
+                            </CardHeader>
+
+                            <CardContent>
+                                <Title>Carteira Virtual</Title>
+                                <Description>R$ 12.452,85</Description>
+                            </CardContent>
+
+                            <CardFooter>
+                                R$ 130,90 foram adicionados a sua carteira recentemente
+                            </CardFooter>
+                        </Card>
+                    </Content>
+                    <Tabs />
+            </InnerContainer>
+    );
+};
+
+const MyTextInput = ({label, icon, isPassword, hidePassword, setHidePassword, ...props}) => {
+    return(
+        <View>
+            <LeftIcon>
+                <Octicons name={icon} size={30} color={brand}/>
+            </LeftIcon>
+            <StyledInputLabel>{label}</StyledInputLabel>
+            <StyledTextInput {...props}/>
+            {isPassword && (
+                <RightIcon onPress={() => setHidePassword(!hidePassword)}>
+                    <Ionicons name = {hidePassword ? 'md-eye-off' : 'md-eye'} size={27} color={darkLight} />
+                </RightIcon>
+            )}
+
+        </View>
+    )
+}
+
+export default Home;
