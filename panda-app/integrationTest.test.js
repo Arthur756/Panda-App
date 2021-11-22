e7b1b2265ed5b63c5406202421b3820d0047aa91
Lucas Newlands <58925749+newlandslucas@users.noreply.github.com>
2021-11-22 03:24:47 -0300

    feature: integration tests

--- panda-app/integrationTest.test.js
+++ panda-app/integrationTest.test.js
@@ -1,7 +1,9 @@
 import integration from './integration';
+import integration1 from './integration';
+
 // integration test 
 
-test('properly return in console Hello World', () => {
-    expect(integration("Hello", "World")).toEqual("Hello World")
+test('properly return in console Hello Test', () => {
+    expect(integration("Hello", "Test")).toEqual("Hello Test")
 })
 
